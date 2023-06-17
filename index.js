b67bc0db93dbd3af140ee5b3b7ac3c03a025350b
Helder <helderlucau@gmail.com>
2023-06-17 22:39:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+//Monday Work function 
+function mondayWork(defActivity = "go to the office"){
+    return `This Monday, I will ${defActivity}.`;
+}
+mondayWork();
+mondayWork('work from home');
+//Wrap Adjective
+function wrapAdjective(operator = '||'){
+     const innerFunction = function (adjective = "a dedicated programmer") {
+        return `You are ${operator}${adjective}${operator}!`;
+    };
+    return innerFunction;
+}
+
+function outer(msg = "It's a fine day to learn") {
+    return function (name, lang = "Python") {
+      return `Hello, ${name}! ${msg} ${lang}`;
+    };
+  }
+  outer("student", "JavaScript");
